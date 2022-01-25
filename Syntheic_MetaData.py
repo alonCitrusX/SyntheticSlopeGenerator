39a19a75be7146f8e35fb319ae429d62140ed709
alonCitrusX <alon@citrusx.ai>
2022-01-25 16:48:25 +0200

    created the Synthetic meta-data classes

---
+++ Syntheic_MetaData.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import List
+import numpy as np
+
+
+class Segment:
+    def __init__(self, d, b_low, b_high, m_low, m_high):
+        self.b_low = b_low
+        self.b_high = b_high
+        self.coef_A = np.random.rand(d)
+        self.coef_b = np.random.rand(1)
+        self.m = np.random.randint(m_low, m_high)
+
+
+class Type(Enum):
+    BINARY = 1
+    CONTINUOUS = 2
+
+
+@dataclass
+class MetaData:
+    d: int
+    segments: List[Segment(d ,0 ,0 ,0 ,0)] #find how to define the boundaries
+    noise: int = 0
+    type = Enum
